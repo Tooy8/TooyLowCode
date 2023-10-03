a8bf8025b45148421514bdcec70f4375d11ed44e
y <13192706165@163.com>
2023-10-03 18:00:58 +0800

    删除注释.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,4 @@
-<!--
- * @Description: What's this for
- * @Autor: WangYuan
- * @Date: 2021-05-19 09:49:33
- * @LastEditors: WangYuan
- * @LastEditTime: 2022-02-11 10:13:04
--->
+
 <template>
   <div id="app">
     <router-view />
