b77e27ed454fb78bb48e0ccf794e429b19c7c2af
y <13192706165@163.com>
2023-10-16 09:53:03 +0800

    删除注释

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,4 @@
-/*
- * @Description: What's this for
- * @Autor: WangYuan
- * @Date: 2021-07-19 09:28:37
- * @LastEditors: WangYuan
- * @LastEditTime: 2021-07-20 10:15:56
- */
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
