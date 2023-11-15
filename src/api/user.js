259b295a4abf95e25e231951e9d90e88e205352e
y <13192706165@163.com>
2023-11-15 09:50:29 +0800

    删除注释

--- src/api/user.js
+++ src/api/user.js
@@ -1,9 +1,6 @@
 /*
  * @Description: 用户api
- * @Autor: WangYuan
- * @Date: 2021-09-22 16:22:01
- * @LastEditors: WangYuan
- * @LastEditTime: 2021-09-22 16:27:10
+
  */
 
 import request from '@/utils/request'
