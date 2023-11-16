866e4192982585e34bd24dd2f77defbef09e0186
y <13192706165@163.com>
2023-11-16 10:20:28 +0800

    删除注释

--- src/components/global/ConfigCtn.vue
+++ src/components/global/ConfigCtn.vue
@@ -1,9 +1,6 @@
 <!--
  * @Description: 属性面板容器
- * @Autor: WangYuan
- * @Date: 2021-05-25 14:41:42
- * @LastEditors: WangYuan
- * @LastEditTime: 2022-01-18 16:20:16
+
 -->
 <template>
   <div class="decorate">
@@ -31,13 +28,17 @@ export default {
   top: 0;
   right: 0;
   bottom: 0;
-  width: 360px; /*no*/
+  width: 360px;
+  /*no*/
   background: #fff;
 
   .decorate-head {
-    height: 70px; /*no*/
-    line-height: 70px; /*no*/
-    padding-left: 12px; /*no*/
+    height: 70px;
+    /*no*/
+    line-height: 70px;
+    /*no*/
+    padding-left: 12px;
+    /*no*/
     border-bottom: 1px solid #f2f4f6;
     font-size: 18px;
     font-weight: 600;
@@ -46,13 +47,14 @@ export default {
   }
 
   .decorate-body {
-    height: calc(100% - 70px); /*no */
+    height: calc(100% - 70px);
+    /*no */
     overflow: auto;
     padding-top: 20px;
 
     &::-webkit-scrollbar {
-      display: none; /* Chrome Safari */
+      display: none;
+      /* Chrome Safari */
     }
   }
-}
-</style>
+}</style>
