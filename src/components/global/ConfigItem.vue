3b066e6913825560f1efcb4621eea205ca930931
y <13192706165@163.com>
2023-11-17 10:18:32 +0800

    删除注释

--- src/components/global/ConfigItem.vue
+++ src/components/global/ConfigItem.vue
@@ -1,13 +1,7 @@
-<!--
- * @Description: What's this for
- * @Autor: WangYuan
- * @Date: 2021-06-03 15:05:00
- * @LastEditors: WangYuan
- * @LastEditTime: 2021-10-18 15:20:57
--->
+
 <template>
   <div class="flex p10 bg-white">
-    <div class="w70 f13 f-grey pt10 ">{{label}}</div>
+    <div class="w70 f13 f-grey pt10 ">{{ label }}</div>
     <div class="flex-1">
       <slot></slot>
     </div>
